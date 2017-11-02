446fe0c9b63ccb25aadac7e89f8f2415b51b7d44
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-02 17:17:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -41,8 +41,7 @@ class App < Sinatra::Base
     session[:id] = 1
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
-      "You did it! session[:id] value: .\nNow, clear the session in the '/logout' route.\nSee the readme for further instruct
-      ions.\nThen, navigate to the '/logout' path."
+      
       redirect '/fetch_session_id'
     else
       "Session ID has not been set!"
